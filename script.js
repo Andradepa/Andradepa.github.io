eac233875734df3744409e9ed92356da7cb45200
Andradepa <163591177+Andradepa@users.noreply.github.com>
2024-05-18 11:34:32 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+
+function appendNumber(number) {
+    const display = document.getElementById('display');
+    display.value += number;
+}
+
+function appendOperator(operator) {
+    const display = document.getElementById('display');
+    display.value += ` ${operator} `;
+}
+
+function clearDisplay() {
+    const display = document.getElementById('display');
+    display.value = '';
+}
